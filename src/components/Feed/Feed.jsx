863fd3f92fb5ee587db2c677e1154f3f5cb574e3
LineCoran <vppisya@gmail.com>
2023-03-02 21:26:30 +0300

    feat: create data for posts

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,19 +1,28 @@
 import Post from '../Post/Post';
 import Share from '../Share/Share';
 import './Feed.css';
+import { Posts } from '../../dummyData';
+import { Users } from '../../dummyData';
 
 export default function Feed() {
   return (
     <div className='feed'>
       <div className="feedWrapper">
         <Share />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {
+          Posts.map((post) => {
+            const {username, profilePicture} = Users.filter(user => user.id === post.userId)[0];
+            return <Post
+              key={post.id}
+              date={post.date}
+              desc={post.desc}
+              like={post.like}
+              comment={post.comment}
+              username={username}
+              profilePicture={profilePicture}
+              photo={post.photo}/>
+          }   
+        )}
         <Post />
       </div>
     </div>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,32 +1,32 @@
 import './Post.css'
 import { MoreVert } from '@mui/icons-material';
 
-export default function Post() {
+export default function Post({date, desc = '', like, comment, photo, username, profilePicture}) {
   return (
     <div className='post'>
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className='postProfileImage' src="assets/person/7.jpeg" alt="" />
-            <span className='postUserName'>Alexey Kuzmichev</span>
-            <span className='postDate'>5 mins ago</span>
+            <img className='postProfileImage' src={profilePicture} alt="" />
+            <span className='postUserName'>{username}</span>
+            <span className='postDate'>{date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">Hey my it's my first post. I can tell you about my intresting life!</span>
-          <img className='postImage' src="assets/posts/3.jpeg" alt="" />
+          <span className="postText">{desc}</span>
+          <img className='postImage' src={photo} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className='likeIcon' src="assets/like.png" alt="" />
             <img className='likeIcon' src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">32 people like it</span>
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
-            <span className='postComentText'>9 comments</span>
+            <span className='postComentText'>{comment} comments</span>
           </div>
         </div>
       </div>
